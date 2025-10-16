3993e6645645eb9719c59bf973a98454386a9fd7
Maisa Oliveira <maisaolisilva@gmail.com>
2025-10-16 14:08:49 -0300

    correção de erro

--- app/componentes/listaCards/card/page.tsx
+++ app/componentes/listaCards/card/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { Card, CardContent, Typography, Box } from "@mui/material";
 
--- app/componentes/listaProjetos/cardProjeto/page.tsx
+++ app/componentes/listaProjetos/cardProjeto/page.tsx
@@ -1,4 +1,4 @@
-"use clinte"
+"use client";
 
 import React, { useState } from "react";
 import {
